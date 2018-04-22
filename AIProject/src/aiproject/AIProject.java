b73dbde8269752015fe8e41eaa9aaa35fa81999b
Htoo <hm1487@nyu.edu>
2018-04-22 14:14:10 -0400

    Logic Updates
    Can Now only move white pieces
    Started on AI
    Created list of legal moves that can be taken by the ai

--- AIProject/src/aiproject/AIProject.java
+++ AIProject/src/aiproject/AIProject.java
@@ -32,34 +32,62 @@ public class AIProject {
     static boolean takenPiece = false;
     static int redPlayerCounter = 0;
     static int blackPlayerCounter = 0;
-    public boolean logicChecker(JFrame jf){
-        return true;
-    }
+    final int maxUtilityValue = -1000;
+    final int minUtilityValue = 1000;
     
-    public class Node{
-        int data;
-        List<Node> children;
-    }
-    
-    public class Tree{
-        Node root;
-    }
-    
-    public class MiniMax{
+    //Need to create an arraylist of possible actions that the AI can take
+    //Also need to make sure pieces cannot move backwards
+    public class AlphaBeta{
+        ArrayList<JButton> actions;
         
+        AlphaBeta(ArrayList<JButton> moves){
+            actions = moves;
+        }
         private ArrayList<Integer> validMoves(){
             ArrayList<Integer> holder = new ArrayList();
             
             return holder; 
         }
-        
-        
-        private void move(int depth, String color, boolean maxPlayer){
-            double alpha = Double.NEGATIVE_INFINITY;
-            double beta = Double.POSITIVE_INFINITY;
+        public void alphaBetaSearch(){
+            int value = maxValue(maxUtilityValue,minUtilityValue);
+            //Actual taking of action goes here
+            
         }
+        public int maxValue(int alpha, int beta){
+            int value = maxUtilityValue;
+            
+            return value;
+        }
+        
+        public int minValue(int alpha, int beta){
+            int value = minUtilityValue;
+            
+            return value;
+        }
+        
+       
     }
     
+    public static ArrayList<JButton> legalMoves(){
+        ArrayList<JButton> temp = new ArrayList();
+        for (int x = 0; x < al.size(); x++){
+            if (al.get(x).getForeground()==Color.black){
+                if (al.get(x+5).getText() != "O"){
+                    temp.add(al.get(x+5));
+                }
+                if (al.get(x+7).getText() != "O"){
+                    temp.add(al.get(x+5));
+                }
+                if (al.get(x+5).getText().equals("O") && !(al.get(x+5).getForeground().equals(Color.black))){
+                    temp.add(al.get(x+10));
+                }
+                if (al.get(x+7).getText().equals("O") && !(al.get(x+7).getForeground().equals(Color.black))){
+                    temp.add(al.get(x+14));
+                } 
+            }
+        }
+        return temp;
+    }
     
     public static boolean legalMove(JButton x){
         int destination = al.indexOf(x);
@@ -158,9 +186,12 @@ public class AIProject {
                 x.setBackground(Color.red);
                 if (counter < 13)
                     x.setText("O");
-                if (counter > 27){
+                else if (counter > 27){
                     x.setText("O");
                     x.setForeground(Color.white);
+                }
+                else{
+                    x.setForeground(null);
                 }
             }
             else{
@@ -177,8 +208,12 @@ public class AIProject {
     static class MyButtonAction implements ActionListener{
         public void actionPerformed(ActionEvent e){
             JButton jb = (JButton) e.getSource();
-            if (!jb.getText().equals("") || jb.getText().equals("") && holdingPiece == true){
-                if (holdingPiece == false){
+            if (!jb.getText().equals("") || (jb.getText().equals("") && holdingPiece == true)){
+                if (jb.getForeground().equals(Color.black) && holdingPiece == false){
+                    System.out.println("That piece is Black! Yuck!");
+                    holdingPiece = false;
+                } 
+                else if (holdingPiece == false && !jb.getText().equals("") ){
                     holdingPiece = true;
                     holder = jb;
                 }
@@ -188,15 +223,9 @@ public class AIProject {
                         holder.setText("");
                         jb.setForeground(Color.white);
                         moveCounter += 1;
+                        holdingPiece = false;
                     }
-                    else{
-                        jb.setText("O");
-                        holder.setText("");
-                        jb.setForeground(Color.black);
-                        moveCounter+=1;
-                    }
-                    holdingPiece = false;
-                        
+   
                 }
             }
         }
