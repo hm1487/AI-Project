684a4421b05db8481afc1ad02a6efec4a6d34773
Htoo <hm1487@nyu.edu>
2018-04-22 16:18:33 -0400

    ValidMoves
    Added valid move functionality (IT WORKS HAHAHA)

--- AIProject/src/aiproject/AIProject.java
+++ AIProject/src/aiproject/AIProject.java
@@ -71,19 +71,31 @@ public class AIProject {
     public static ArrayList<JButton> legalMoves(){
         ArrayList<JButton> temp = new ArrayList();
         for (int x = 0; x < al.size(); x++){
-            if (al.get(x).getForeground()==Color.black){
-                if (al.get(x+5).getText() != "O"){
-                    temp.add(al.get(x+5));
+            if (al.get(x).getForeground().equals(Color.black) && al.get(x).getText().equals("O")){
+                if (x+5 < al.size()){
+                    if (!al.get(x+5).getText().equals("O") && !al.get(x+5).getBackground().equals(Color.black)){
+                        //System.out.println("Break1");
+                        temp.add(al.get(x+5));
+                    }
                 }
-                if (al.get(x+7).getText() != "O"){
-                    temp.add(al.get(x+5));
+                if (x+7 <al.size() && !al.get(x+5).getBackground().equals(Color.black)){
+                    if (!al.get(x+7).getText().equals("O")){
+                        //System.out.println("Break1");
+                        temp.add(al.get(x+5));
+                    }
                 }
-                if (al.get(x+5).getText().equals("O") && !(al.get(x+5).getForeground().equals(Color.black))){
-                    temp.add(al.get(x+10));
+                if (x + 10 < al.size()){
+                    if (!al.get(x+10).getText().equals("O") && (al.get(x+5).getForeground().equals(Color.white))){
+                        System.out.println("Break1");
+                        temp.add(al.get(x+10));
+                    }
                 }
-                if (al.get(x+7).getText().equals("O") && !(al.get(x+7).getForeground().equals(Color.black))){
-                    temp.add(al.get(x+14));
-                } 
+                if (x + 14 < al.size()){
+                    if (!al.get(x+14).getText().equals("O") && (al.get(x+7).getForeground().equals(Color.white))){
+                        System.out.println("Break2");
+                        temp.add(al.get(x+14));
+                    } 
+                }
             }
         }
         return temp;
@@ -127,7 +139,6 @@ public class AIProject {
             }
         }
         else if (al.get(origin-7).getText().equals("O") && !(al.get(origin-7).getForeground().equals(holder.getForeground()))){
-            System.out.println("We got to break 3");
             if (destination - origin == -14 || !x.getText().equals("O")){
                 al.get(origin-7).setText("");
                 if (holder.getForeground() == Color.white){
@@ -142,7 +153,6 @@ public class AIProject {
             }
         }
         else if (al.get(origin-5).getText().equals("O") && !(al.get(origin-5).getForeground().equals(holder.getForeground()))){
-            System.out.println("We got to break 4");
             if (destination-origin == -10 || !x.getText().equals("O")){
                 al.get(origin-5).setText("");
                 if (holder.getForeground() == Color.white){
@@ -208,6 +218,7 @@ public class AIProject {
     static class MyButtonAction implements ActionListener{
         public void actionPerformed(ActionEvent e){
             JButton jb = (JButton) e.getSource();
+            System.out.println(jb);
             if (!jb.getText().equals("") || (jb.getText().equals("") && holdingPiece == true)){
                 if (jb.getForeground().equals(Color.black) && holdingPiece == false){
                     System.out.println("That piece is Black! Yuck!");
@@ -224,6 +235,10 @@ public class AIProject {
                         jb.setForeground(Color.white);
                         moveCounter += 1;
                         holdingPiece = false;
+                        
+                        ArrayList<JButton> temporary = new ArrayList();
+                        temporary = legalMoves();
+                        System.out.println(temporary.size());
                     }
    
                 }
